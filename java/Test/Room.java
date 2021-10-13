b4734c4a21bd1c88bad32094e23bdee2d5c223d9
Tomáš Sabol <tomassabol2@gmail.com>
2021-10-13 23:22:48 +0200

    Update Room.java

--- java/Test/Room.java
+++ java/Test/Room.java
@@ -82,5 +82,15 @@ public class Room
         }
     }
     
+    public void experienceRange(int value1, int value2){
+        this.value1 = value1;
+        this.value2 = value2;
+        
+        for (Attendee attendee : attendees){
+            if (value 1 < attendee.getExperience < value 2){
+                System.out.println(attendee.getName());
+            }
+        }
+    }
     
 }
